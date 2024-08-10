c9e341e8792f94d8bff537212657850379eec133
techbd123 <techbd123@gmail.com>
2024-08-10 09:03:26 +0600

    Solved the Assignment1

---
+++ Assignments/Assignment1/script.js
@@ -0,0 +1,33 @@
+function generate()
+{
+    let val = 100000 + Math.round(899999*Math.random());
+    document.getElementById("textbox1").setAttribute("value", "" + val);
+}
+
+function pressed(parameter)
+{
+    let prev = document.getElementById("textbox2").getAttribute("value");
+    if(parameter == "submit")
+    {
+        let v1 = document.getElementById("textbox1").getAttribute("value");
+        if(v1 == prev) document.getElementById("output").innerHTML= "&#x2705; Your Secret Key is matched!";
+        else
+        {
+            document.getElementById("output").innerHTML="&#x274E; Your Secret Key is NOT matched!";
+        }
+        return ;
+    }
+    if(parameter == "backspace")
+    {
+        prev = prev.substring(0, prev.length-1);
+    }
+    else if(parameter == "clear")
+    {
+        prev = "";
+    }
+    else 
+    {
+        prev += "" + parameter;
+    }
+    document.getElementById("textbox2").setAttribute("value", prev);
+}
